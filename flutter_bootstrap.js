303055b743f400a60fd05b7a52409448f5e48637
vi01afta-lab <vi01afta@gmail.com>
2026-07-09 02:29:12 +0000

    Deploying to gh-pages from @ vi01afta-lab/calisthenics-level-up@479051342c26c856bb6220b00a09baff2525c63c 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "297883443" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1548482582" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
